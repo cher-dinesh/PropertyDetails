ac72d14d2d7ac944fa86b3ea8abc4b32ae667c0c
cher-dinesh <139626471+cher-dinesh@users.noreply.github.com>
2024-01-04 16:48:55 +0000

    Update

--- src/Components/PropertyDetails.jsx
+++ src/Components/PropertyDetails.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
 import axios from 'axios';
-import './PropertyDetails.css'
+import Image from '../Assets/PD.jpg';
+import './PropertyDetails.css';
+import EnquiryPopup from './EnquiryPopup';
 
 const PropertyDetails = () => {
   const [propertyData, setPropertyData] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
   const apiUrl = 'http://13.234.238.86/api/getpropertydetails?ids=[3]';
-  const locationApiUrl='http://13.234.238.86/api/getlocationsbyids';
+  const locationApiUrl = 'http://13.234.238.86/api/getlocationsbyids';
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +30,7 @@ const PropertyDetails = () => {
           id: rawData[0].id.value,
           name: rawData[0].name,
           description: rawData[0].description,
-          price: rawData[0].price,
+          price: (rawData[0].price / 100000).toFixed(2),
           type: rawData[0].type,
           location: rawData[0].location.value,
           imgext: rawData[0].imgext || 'N/A',
@@ -53,31 +57,32 @@ const PropertyDetails = () => {
     };
     fetchData();
   }, [locationApiUrl]);
+
   const generateImageUrl = () => {
     const { id, imgext } = propertyData;
     const len = imgext.length;
-    return len > 1 ? `http://13.234.238.86/media/prop/${id}_${2}.jpeg` : `http://13.234.238.86/media/prop/${id}.jpeg`;
+    return len > 1 ? `http://13.234.238.86/media/prop/${id}_2.jpeg` : `http://13.234.238.86/media/prop/${id}.jpeg`;
   };
-  
+
+  const handleEnquireClick = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div>
-      <h1>Property Details</h1>
       {propertyData ? (
         <div className="property-listing">
-          <nav className="nav-container">
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-256/free-global-navigation-bar-1174857.png"
-              className="nav-icon"
-              alt="navigation icon"
-            />
-          </nav>
+          <Navbar />
           <div className="container">
             <div className="sub-container">
-              <h1>{propertyData.name}</h1>
+              <h1 className='heading'>{propertyData.name}</h1>
               <div className="details-container">
                 <div className="image-slider">
-                  <img src={generateImageUrl()} className="image" alt="Home" />
+                  <img src={generateImageUrl()} className="image" alt={Image} />
                 </div>
                 <div className="details">
                   <div className="description">
@@ -92,7 +97,7 @@ const PropertyDetails = () => {
                 </div>
               </div>
               <div className="btn-container">
-                <button className="enquire-btn">Enquire Now</button>
+                <button className="enquire-btn" onClick={handleEnquireClick} type='submit'>Enquire Now</button>
               </div>
             </div>
           </div>
@@ -100,9 +105,11 @@ const PropertyDetails = () => {
       ) : (
         <p>Loading...</p>
       )}
+      {showPopup && (
+        <EnquiryPopup onClose={closePopup} locationId={propertyData.location} />
+      )}
     </div>
   );
-  
 };
 
 export default PropertyDetails;
